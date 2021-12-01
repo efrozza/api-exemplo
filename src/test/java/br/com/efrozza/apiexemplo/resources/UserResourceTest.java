a39f2f499704305f0d67748166e4699a23419baf
Everton Frozza <evertonfrozza@gmail.com>
2021-12-01 10:51:31 -0300

    teste atualizarUsuario do UserResource

--- src/test/java/br/com/efrozza/apiexemplo/resources/UserResourceTest.java
+++ src/test/java/br/com/efrozza/apiexemplo/resources/UserResourceTest.java
@@ -96,7 +96,19 @@ class UserResourceTest {
     }
 
     @Test
-    void atualizarUsuario() {
+    void whenAtualizarUsuario() {
+
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.atualizarUsuario(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
     }
 
     @Test
